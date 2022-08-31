077d08850e188ee412a395dba9f5751446ac4daa
nekokoe <nekokoe@qq.com>
2022-08-31 14:35:09 +0800

    update scripts

--- scripts/batch_plasmer.py
+++ scripts/batch_plasmer.py
@@ -2,11 +2,9 @@ import sys
 import os
 
 falist = sys.argv[1].strip()
-script = sys.argv[2].strip()
-model = sys.argv[3].strip()
-kmerdb = sys.argv[4].strip()
-threads = sys.argv[5].strip()
-bash = sys.argv[6].strip()
+kmerdb = sys.argv[2].strip()
+threads = sys.argv[3].strip()
+bash = sys.argv[4].strip()
 
 w = open(bash,"w")
 with open(falist) as r:
@@ -14,6 +12,6 @@ with open(falist) as r:
 		line = line.strip()
 		outdir = "/".join(line.split("/")[0:len(line.split("/"))-1])
 		prefix = line.split("/")[-1]
-		w.write("bash {} -g {} -p {} -m {} -d {} -t {} -o {}".format(script,line,prefix,model,kmerdb,threads,outdir) + "\n")
+		w.write("Plasmer -g {} -p {} -d {} -t {} -o {}".format(line,prefix,kmerdb,threads,outdir) + "\n")
 
 w.close()
